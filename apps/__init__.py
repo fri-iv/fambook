25c30fbc3a6f40db37d1a2d78a402fa0791fd9f8
Maksym Stukalo <stukalo.maksym@gmail.com>
2017-05-05 23:01:01 +0200

    added static folder

--- apps/__init__.py
+++ apps/__init__.py
@@ -4,7 +4,10 @@ import os
 from flask import Flask
 
 template_dir = os.path.abspath('templates')
-app = Flask(__name__, template_folder=template_dir)
+static_dir = os.path.abspath('static')
+app = Flask(__name__,
+            template_folder=template_dir,
+            static_folder=static_dir)
 app.config.from_object(__name__)
 app.secret_key = 'SdfhP(DS&yfpSIUdfgsidfhSFIUSgdfs'
 
